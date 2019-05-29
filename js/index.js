71a7d22607f5391731ffaf8e4ccc481d8fcf92f9
Graphic <graphic@WFD201-00M.local>
2019-05-29 09:40:24 -0400

    Dynamically add the first character from an array

--- js/index.js
+++ js/index.js
@@ -1,19 +1,3 @@
-// // An "Array" of characters (This is as single column)
-// const favCharNames = [
-//   `Tyrian`,  // 0
-//   `Sansa`,   // 1
-//   `Bron`,    // 2
-//   `Brianne`,  // 3
-// ];
-
-// // One character as an Object (This is a single row)
-// const oneCharacter = {
-//   name: `Tyrian`,
-//   house: `Lannister`,
-//   actor: `Peter Dinklage`,
-//   status: `alive`,
-// };
-
 // Combine rows and columns... we get a TABLE!
 const allCharacters = [
   { name: `Tyrian`,  house: `Lannister`,  actor: `Peter Dinklage`,      status: `alive` },  // 0
@@ -22,17 +6,24 @@ const allCharacters = [
   { name: `Brianne`, house: `Tarth`,      actor: `Gwendoline Christie`, status: `alive` },  // 3
 ];
 
+// Create a variable to hold which character I want
+let i = 0;
 
-// Example of an expanded character (for reference)
-// const oneCharacter = {
-//   name: `Tyrian`,
-//   house: `Lannister`,
-//   actor: `Peter Dinklage`,
-//   status: `alive`,
-//   nicknames: [ `Tyrian the brave`, `Tyrian the tall` ],
-//   spouses: [
-//     { name: `Sansa`, start: `??`, end: `??`},
-//     { name: `Not sure`, start: `??`, end: `??`},
-//     { name: `Someone else`, start: `??`, end: `??`},
-//   ]
-// };
+// Put the first object into a variable
+const char = allCharacters[i]; 
+
+// Store the location of the list <ol>
+const $favChar = document.getElementById(`favChar`);
+
+// Put one character into the document
+$favChar.innerHTML = `
+  <li>
+    <h2>${char.name} of House ${char.house}</h2>
+    <ul>
+      <li>Played By: ${char.actor}</li>
+      <li>Status: ${char.status}</li>
+    </ul>
+  </li>`;
+
+      
+
